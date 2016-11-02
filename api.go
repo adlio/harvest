6d98d227079deb179a8b15c4085a6628637c7e27
Aaron Longwell <aaron@culturefoundry.com>
2016-11-01 22:21:20 -0700

    Progress on API Get().

--- api.go
+++ api.go
@@ -2,22 +2,49 @@ package harvest
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
 )
 
 const HARVEST_DOMAIN = "harvestapp.com"
 
 type API struct {
+	client    *http.Client
 	BaseURL   string
 	SubDomain string
 	User      string
 	Password  string
 }
 
-func NewBasicAuthAPI(subdomain, user, password string) API {
+func NewBasicAuthAPI(subdomain, user, password string) *API {
 	a := API{}
 	a.SubDomain = subdomain
 	a.User = user
 	a.Password = password
 	a.BaseURL = fmt.Sprintf("https://%s.%s", subdomain, HARVEST_DOMAIN)
-	return a
+	return &a
+}
+
+func (a *API) Get(path string, args Arguments, target interface{}) error {
+	url := fmt.Sprintf("%s/%s", a.BaseURL, path)
+	urlWithParams := fmt.Sprintf("%s?%s", url, args.ToURLValues().Encode())
+
+	req, err := http.NewRequest("GET", urlWithParams, nil)
+	if err != nil {
+		return errors.Wrapf(err, "Invalid GET request %s", url)
+	}
+
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return errors.Wrapf(err, "HTTP request failure on %s", url)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		return errors.Errorf("HTTP request failure on %s: %s %s", url, string(body), err)
+	}
+
+	return nil
 }
---
+++ arguments.go
@@ -0,0 +1,19 @@
+package harvest
+
+import (
+	"net/url"
+)
+
+type Arguments map[string]string
+
+func Defaults() Arguments {
+	return make(Arguments)
+}
+
+func (args Arguments) ToURLValues() url.Values {
+	v := url.Values{}
+	for key, value := range args {
+		v.Set(key, value)
+	}
+	return v
+}
