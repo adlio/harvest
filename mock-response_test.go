95c1c16ef8e73b3a4e10afb72e7678347fdabee7
Aaron Longwell <aaron@culturefoundry.com>
2017-03-25 15:29:15 -0700

    Adds mockDynamicPathResponse(), which can be used to mock dynamic and looping HTTP requests.

--- mock-response_test.go
+++ mock-response_test.go
@@ -1,11 +1,15 @@
 package harvest
 
 import (
+	"crypto/md5"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 func mockResponse(paths ...string) *httptest.Server {
@@ -18,6 +22,36 @@ func mockResponse(paths ...string) *httptest.Server {
 	}
 	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write(mockData)
+	}))
+}
+
+func mockDynamicPathResponse() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+		// Build the path for the dynamic content
+		parts := []string{".", "testdata"}
+		parts = append(parts, strings.Split(strings.TrimPrefix(r.URL.Path, "/"), "/")...)
+		// Remove security strings
+		queryStringPart := r.URL.RawQuery
+		if queryStringPart != "" {
+			parts[len(parts)-1] = fmt.Sprintf("%s-%x", parts[len(parts)-1], md5.Sum([]byte(queryStringPart)))
+		}
+		parts[len(parts)-1] = parts[len(parts)-1] + ".json"
+		filename := filepath.Join(parts...)
+
+		fmt.Println(filename)
+
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			http.Error(rw, fmt.Sprintf("%s doesn't exist. Create it with the mock you'd like to use.\n Args were: %s", filename, queryStringPart), http.StatusNotFound)
+			return
+		}
+
+		mockData, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rw.Write(mockData)
+
 	}))
 }
 
