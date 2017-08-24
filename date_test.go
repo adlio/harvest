9b726cdfef37b31e241bfde9416f7c3b4ce5c321
Aaron Longwell <aaron@culturefoundry.com>
2017-08-23 22:51:24 -0700

    Date.Matches() test coverage.

--- date_test.go
+++ date_test.go
@@ -16,3 +16,21 @@ func TestDateMarshallJSON(t *testing.T) {
 		t.Errorf("Expected '\"2017-03-01\"'. Got '%s'", string(jsonBytes))
 	}
 }
+
+func TestDateMatchesTrue(t *testing.T) {
+	t1, _ := time.Parse("2006-01-02", "2017-03-01")
+	t2 := t1.Add(time.Minute * time.Duration(30))
+	d := Date{t1}
+	if d.Matches(t2) != true {
+		t.Errorf("Date.Matches() should be true as long as the date parts match")
+	}
+}
+
+func TestDateMatchesFalse(t *testing.T) {
+	t1, _ := time.Parse("2006-01-02", "2017-03-01")
+	t2, _ := time.Parse("2006-01-02", "2017-03-02")
+	d := Date{t1}
+	if d.Matches(t2) != false {
+		t.Errorf("Date.Matches() should be fale when date parts are different")
+	}
+}
