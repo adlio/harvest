db760ce4fc23fa927b5211e2d0f79607954c9346
Aaron Longwell <aaron@culturefoundry.com>
2017-09-18 15:02:34 -0700

    Converts InvoiceMessage{} to Harvest 2.0 format.

--- invoice-message.go
+++ invoice-message.go
@@ -5,8 +5,14 @@ import (
 	"time"
 )
 
-type InvoiceMessageResponse struct {
-	InvoiceMessage *InvoiceMessage `json:"message"`
+type InvoiceMessagesResponse struct {
+	InvoiceMessages []*InvoiceMessage `json:"invoice_messages"`
+	PerPage         int64             `json:"per_page"`
+	TotalPages      int64             `json:"total_pages"`
+	TotalEntries    int64             `json:"total_entries"`
+	NextPage        *int64            `json:"next_page"`
+	PreviousPage    *int64            `json:"previous_page"`
+	Page            int64             `json:"page"`
 }
 
 type InvoiceMessage struct {
@@ -27,19 +33,16 @@ type InvoiceMessage struct {
 	FullRecipientList string    `json:"full_recipient_list"`
 }
 
-func (a *API) GetInvoiceMessages(invoiceID int64, args Arguments) (invoicemessages []*InvoiceMessage, err error) {
-	invoiceMessagesResponse := make([]*InvoiceMessageResponse, 0)
+func (a *API) GetInvoiceMessages(invoiceID int64, args Arguments) (invoiceMessages []*InvoiceMessage, err error) {
+	invoiceMessagesResponse := InvoiceMessagesResponse{}
 	path := fmt.Sprintf("/invoices/%v/messages", invoiceID)
 	err = a.Get(path, args, &invoiceMessagesResponse)
-	for _, m := range invoiceMessagesResponse {
-		invoicemessages = append(invoicemessages, m.InvoiceMessage)
-	}
-	return invoicemessages, err
+	return invoiceMessagesResponse.InvoiceMessages, err
 }
 
-func (a *API) GetInvoiceMessage(invoiceID int64, messageID int64, args Arguments) (invoicemessage *InvoiceMessage, err error) {
-	invoiceMessageResponse := InvoiceMessageResponse{}
+func (a *API) GetInvoiceMessage(invoiceID int64, messageID int64, args Arguments) (invoiceMessage *InvoiceMessage, err error) {
+	invoiceMessage = &InvoiceMessage{}
 	path := fmt.Sprintf("/invoices/%v/messages/%v", invoiceID, messageID)
-	err = a.Get(path, args, &invoiceMessageResponse)
-	return invoiceMessageResponse.InvoiceMessage, err
+	err = a.Get(path, args, invoiceMessage)
+	return invoiceMessage, err
 }
