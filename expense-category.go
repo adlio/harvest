81b878527bff13e20c18a171e6ea99565d8a6007
Aaron Longwell <aaron@culturefoundry.com>
2017-09-18 15:01:30 -0700

    Converts ExpenseCategory{} to Harvest 2.0 format.

--- expense-category.go
+++ expense-category.go
@@ -5,12 +5,18 @@ import (
 	"time"
 )
 
-type ExpenseCategoryResponse struct {
-	ExpenseCategory *ExpenseCategory `json:"expense_category"`
+type ExpenseCategoriesResponse struct {
+	ExpenseCategories []*ExpenseCategory `json:"expense_categories"`
+	PerPage           int64              `json:"per_page"`
+	TotalPages        int64              `json:"total_pages"`
+	TotalEntries      int64              `json:"total_entries"`
+	NextPage          *int64             `json:"next_page"`
+	PreviousPage      *int64             `json:"previous_page"`
+	Page              int64              `json:"page"`
 }
 
 type ExpenseCategory struct {
-	ID          int       `json:"id"`
+	ID          int64     `json:"id"`
 	Name        string    `json:"name"`
 	UnitName    string    `json:"unit_name"`
 	UnitPrice   float64   `json:"unit_price"`
@@ -19,19 +25,16 @@ type ExpenseCategory struct {
 	Deactivated bool      `json:"deactivated"`
 }
 
-func (a *API) GetExpenseCategory(expensecategoryID int64, args Arguments) (expensecategory *ExpenseCategory, err error) {
-	expenseCategoryResponse := ExpenseCategoryResponse{}
-	path := fmt.Sprintf("/expense_categories/%v", expensecategoryID)
-	err = a.Get(path, args, &expenseCategoryResponse)
-	return expenseCategoryResponse.ExpenseCategory, err
+func (a *API) GetExpenseCategory(expenseCategoryID int64, args Arguments) (expenseCategory *ExpenseCategory, err error) {
+	expenseCategory = &ExpenseCategory{}
+	path := fmt.Sprintf("/expense_categories/%v", expenseCategoryID)
+	err = a.Get(path, args, expenseCategory)
+	return expenseCategory, err
 }
 
-func (a *API) GetExpenseCategories(args Arguments) (expensecategories []*ExpenseCategory, err error) {
-	expenseCategoriesResponse := make([]*ExpenseCategoryResponse, 0)
+func (a *API) GetExpenseCategories(args Arguments) (expenseCategories []*ExpenseCategory, err error) {
+	expenseCategoriesResponse := ExpenseCategoriesResponse{}
 	path := fmt.Sprintf("/expense_categories")
 	err = a.Get(path, args, &expenseCategoriesResponse)
-	for _, er := range expenseCategoriesResponse {
-		expensecategories = append(expensecategories, er.ExpenseCategory)
-	}
-	return expensecategories, err
+	return expenseCategoriesResponse.ExpenseCategories, err
 }
